c898de20334712228829d9484c51aea455c25300
ShahidaMahtab <marwa.mahtab@gmail.com>
2021-10-04 00:16:00 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import banner from "../../Image/banner.jpg";
+
+const Header = () => {
+  return (
+    <div>
+      <img src={banner} alt="" />
+    </div>
+  );
+};
+
+export default Header;
