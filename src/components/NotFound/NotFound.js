a4b533ae683d93e1a310b70b195db9c9de50b280
ShahidaMahtab <marwa.mahtab@gmail.com>
2021-10-04 20:56:40 +0600

    final update

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from "react";
 import notFound from "../../Image/404.png";
 const NotFound = () => {
   return (
-    <div className="text-center mt-5">
+    <div className="text-center my-5 py-5">
       <img src={notFound} alt="error-msg" />
       <h2>Page Not Found</h2>
     </div>
